67fa320bb78c76efcadcd38cdfc04ed2070c2c47
Grace Lee <gracelee9901@gmail.com>
2023-03-05 15:46:26 -0800

    Add basic ui and game logic

--- src/js/battle.js
+++ src/js/battle.js
@@ -1,6 +1,7 @@
 export default battle;
 import {changeState, storeState, stateControl} from './game.js';
 import enemies from './monsters/enemies.js';
+import characters from './classes/character.js';
 
 // states that are changing
 // Montsers:
@@ -23,7 +24,7 @@ import enemies from './monsters/enemies.js';
 //heals that a charater can use
 const lifeJuice = changeState("potion")(10);
 const spiritJuice = changeState("mana")(12);
-//damage that a charater can make
+//damage that a charater can maked
 const physicalAttack = changeState("hands")(1);
 const magicAttack = changeState("wingardium leviosa")(2);
 //damage that an enemy can make
@@ -32,16 +33,58 @@ const physicalDamageMagic = changeState("spooky ouchy")(2);
 
 let scenario = 1
 
+// Link this to choose player in UI
+const selectChar = () => {
+  if(characters === "warrior") {
+    chosenChar = characters[1].type;
+  }
+  else {
+    chosenChar = characters[0].type;
+  }
+}
+
 // pulls enemy from enemies list
 const battleEnemy = () => {
   if(scenario === 1) {
     enemy1 = enemies[0].type;
     startBattle();
   }
-}
+  else if (scenario === 2) {
+    enemy2 = enemies[1].type;
+    startBattle();
+  }
+};
 
-// initait battle
+
+// initiate battle
 const startBattle = () => {
-  
+  let activeEnemy = battleEnemy();
+  let monsterHp = activeEnemy.hp;
+  let monsterName = activeEnemy.type;
+  monsterNameText.innerText = monsterName;
+  monsterHealthText.innerText = monsterHp;
+};
 
+// Attack function
+const attack = () => {
+  let activeChar = selectChar();
+  let health = activeChar.hp;
+  let strenght = activeChar.str;
+  let attackInit = activeChar.def;
+
+  if (activeEnemy.spd >= activeChar.def){
+    health -= activeEnemy.dmg;
+  }
+  else if (activeEnemy.spd <= activeChar.def) {
+    monsterHp -=  strenght;
+  }
+};
+
+const spellCast = () => {
+   if(activeChar === "mage") {
+    monsterHp -= mana;
+   }
+   else {
+    monsterHp -= 0;
+   }
 }
--- src/js/game.js
+++ src/js/game.js
@@ -28,3 +28,37 @@ const stateControl = storeState();
 // - Mana:
 // - Def:
 // - Exp:
+
+// Need function for getting and settings character and base stats
+
+window.addEventListener("load", function() {
+  document.getElementById("phys-attack").onclick = function() {
+    const newState = stateControl(physicalAttack);
+    document.getElementById("phys-attack").innerText = `physical dmage: ${newState.physicalAttack}`;
+  };
+
+  document.getElementById("fertilize").onclick = function() {
+    const newState = stateControl(growJuice);
+    document.getElementById("fertilizer-val").innerText = `Fertilizer: ${newState.fertilizer}`;
+  };
+
+  document.getElementById("light").onclick = function() {
+    const newState = stateControl(photosynthesize);
+    document.getElementById("light-val").innerText = `Light: ${newState.light}`;
+  };
+
+  document.getElementById("waterPlant").onclick = function() {
+    const newState = stateControl(hydrate);
+    document.getElementById("water-val").innerText = `Water: ${newState.water}`;
+  };
+
+  this.document.getElementById()
+
+  document.getElementById("show-state").onClick = function() {
+    const currentState = stateControl();
+    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+    document.getElementsByName('fertilizer-val').innderText = `Fertilizer: ${currentState.fertilizer}`;
+    document.getElementsByName('light-val').innderText = `Light: ${currentState.light}`;
+    document.getElementsByName('water-val').innderText = `Water: ${currentState.water}`;
+  };
+});
