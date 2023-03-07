346769a1286213e538c6b7b5d2252b4103f86f20
Grace Lee <gracelee9901@gmail.com>
2023-03-06 18:59:11 -0800

    Add eslintrc and babelrc files
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

--- src/js/battle.js
+++ src/js/battle.js
@@ -1,4 +1,3 @@
-export default battleEnemy;
 import {changeState, storeState, stateControl} from './game.js';
 import enemies from './monsters/enemies.js';
 import characters from './classes/character.js';
@@ -21,37 +20,40 @@ import characters from './classes/character.js';
 // - Def:
 // - Exp:
 
-//heals that a charater can use
-const lifeJuice = changeState("potion")(10);
-const spiritJuice = changeState("mana")(12);
-//damage that a charater can maked
-const physicalAttack = changeState("enemy-hp")(1);
-const magicAttack = changeState("wingardium leviosa")(2);
-//damage that an enemy can make
-const physicalDamage = changeState("ouchy")(1);
-const physicalDamageMagic = changeState("spooky ouchy")(2);
+// //heals that a charater can use
+// const lifeJuice = changeState("potion")(10);
+// const spiritJuice = changeState("mana")(12);
+// //damage that a charater can maked
+// const physicalAttack = changeState("enemy-hp")(1);
+// const magicAttack = changeState("wingardium leviosa")(2);
+// //damage that an enemy can make
+// const physicalDamage = changeState("ouchy")(1);
+// const physicalDamageMagic = changeState("spooky ouchy")(2);
 
 // let scenario = 1
 
 // Link this to choose player in UI
-const selectChar = () => {
+export const selectChar = () => {
   if(characters === "warrior") {
-    chosenChar = characters[1].type;
+    chosenChar = characters[1];
   }
-  else {
-    chosenChar = characters[0].type;
+  else if (characters === "mage") {
+    chosenChar = characters[0];
   }
 }
 
 // pulls enemy from enemies list
-const battleEnemy = () => {
+export const battleEnemy = () => {
   let randomEnemy = Math.floor(Math.random() * 2);
 
   if (randomEnemy === 0 ){
     activeEnemeyName = enemies[0].type;
+    console.log(activeEnemeyName);
+    return activeEnemeyName;
   }
   else if (randomEnemy === 1 ){
     activeEnemeyName = enemies[1].type;
+    return activeEnemeyName;
   }
   
   // if(scenario === 1) {
--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,11 @@
 export default game;
 import {battleEnemy, selectChar} from './battle.js';
 
-const changeState = (somethingForNow) => {
+const changeState = (prop) => {
   return (value) => {
     return(state) => ({
       ...state,
-      [somethingForNow] : (state[somethingForNow] || 0) + value
+      [prop] : (state[prop] || 0) + value
     });
   };
 };
@@ -21,6 +21,9 @@ const storeState = () => {
 
 const stateControl = storeState();
 
+
+
+
 // states that are changing 
 // - lvl:
 // - HP:
@@ -33,33 +36,44 @@ const stateControl = storeState();
 
 window.addEventListener("load", function() {
   
+  //heals that a charater can use
+  const lifeJuice = changeState("potion")(10);
+  const spiritJuice = changeState("mana")(12);
+  //damage that a charater can maked
+  const physicalAttack = changeState("enemy-hp")(1);
+  const magicAttack = changeState("wingardium leviosa")(2);
+  //damage that an enemy can make
+  const physicalDamage = changeState("ouchy")(1);
+  const physicalDamageMagic = changeState("spooky ouchy")(2);
+
+
   document.getElementById("phys-attack").onclick = function() {
     const newState = stateControl(physicalAttack);
     document.getElementById("enemy-hp").innerText = `physical dmage: ${newState.physicalAttack}`;
   };
 
   document.getElementById("magic-attack").onclick = function() {
-    const newState = stateControl(growJuice);
+    const newState = stateControl(magicAttack);
     document.getElementById("fertilizer-val").innerText = `Fertilizer: ${newState.fertilizer}`;
   };
 
   document.getElementById("life-juice").onclick = function() {
-    const newState = stateControl(photosynthesize);
+    const newState = stateControl(lifeJuice);
     document.getElementById("light-val").innerText = `Light: ${newState.light}`;
   };
 
   document.getElementById("spirit-juice").onclick = function() {
-    const newState = stateControl(hydrate);
+    const newState = stateControl(spiritJuice);
     document.getElementById("water-val").innerText = `Water: ${newState.water}`;
   };
 
   this.document.getElementById()
 
-  document.getElementById("show-state").onClick = function() {
-    const currentState = stateControl();
-    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
-    document.getElementsByName('fertilizer-val').innderText = `Fertilizer: ${currentState.fertilizer}`;
-    document.getElementsByName('light-val').innderText = `Light: ${currentState.light}`;
-    document.getElementsByName('water-val').innderText = `Water: ${currentState.water}`;
-  };
+  // document.getElementById("show-state").onClick = function() {
+  //   const currentState = stateControl();
+  //   document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  //   document.getElementsByName('fertilizer-val').innderText = `Fertilizer: ${currentState.fertilizer}`;
+  //   document.getElementsByName('light-val').innderText = `Light: ${currentState.light}`;
+  //   document.getElementsByName('water-val').innderText = `Water: ${currentState.water}`;
+  // };
 });
